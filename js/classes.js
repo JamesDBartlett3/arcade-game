0a868e1319f1a43d0794c72fb5822d1cad0dfb34
James D. Bartlett III <jimmyb@gmail.com>
2018-06-24 11:02:07 -0500

    Basic file structure & ES6 classes.js

---
+++ js/classes.js
@@ -0,0 +1,32 @@
+class Entity {
+  constructor() {
+    this.sprite = 'images/';
+    this.x = 2;
+    this.y = 5;
+  }
+
+  update() {
+    this.isOutOfBoundsX = this.x > 5;
+    this.isOutOfBoundsY = this.y < 1;
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+  }
+}
+
+class Player extends Entity {
+  constructor() {
+    super();
+    this.sprite += 'char-boy.png';
+  }
+}
+
+class Enemy extends Entity {
+  constructor(x, y) {
+    super();
+    this.sprite = += 'enemy-bug.png';
+    this.x = x;
+    this.y = y;
+  }
+}
