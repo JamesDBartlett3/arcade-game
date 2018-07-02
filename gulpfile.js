d1a43d10711d3e8c3de097d9e5d0eaab3e2f6477
James D. Bartlett III <jimmyb@gmail.com>
2018-07-02 13:36:05 -0500

    added gulp plugins -- browserSync, autoprefixer, sass, and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-  // place code for your default task here
+gulp.task('default', function(done) {
+  console.log('Default Task!')
   done();
-}
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+     server: "./"
+  });
+});
+
+gulp.task('styles', function(done) {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+    done();
+});
+
+gulp.task('watch', ['browserSync', 'styles'], function(done) {
+  gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('js/**/*.js', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+
+});
