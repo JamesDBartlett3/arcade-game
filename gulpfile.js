cc2a8ffe1ead4aea0ef1873337e40af8514bbf68
James D. Bartlett III <jimmyb@gmail.com>
2018-07-02 14:24:08 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    eslint = require('gulp-eslint'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function(done) {
-  console.log('Default Task!')
+gulp.task('default', ['styles', 'lint'], function(done) {
+  gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('js/**/*.js', ['lint']);
   done();
 });
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function(done) {
   browserSync.init({
-     server: "./"
-  });
+     server: './'
+  })
+  done();
+});
+
+gulp.task('lint', function(done) {
+  return gulp.src(['js/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError())
+    done();
 });
 
 gulp.task('styles', function(done) {
